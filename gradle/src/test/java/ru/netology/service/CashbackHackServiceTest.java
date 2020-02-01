0dee925063fa2f0117308db5e15da7fef3828de7
Marina Korobova <marinagaga@gmail.com>
2020-02-01 16:52:24 -0500

    build

--- gradle/src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ gradle/src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,5 +1,26 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CashbackHackServiceTest {
 
+    @Test
+    void shouldReturnCashbackIfAmountIsPositiveAndLess1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 900;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 100;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldNotReturnCashbackIfAmountIsDivisibleBy1000() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
 }
