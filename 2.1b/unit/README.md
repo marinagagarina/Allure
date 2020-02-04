296fc7f95c4236eabcf7fc4c27b0897e29fd1477
Marina Korobova <marinagaga@gmail.com>
2020-02-04 11:25:12 -0500

    fix test

---
+++ 2.1b/unit/README.md
@@ -0,0 +1,3 @@
+Project sample
+
+[![Build status](https://ci.appveyor.com/api/projects/status/c20xt72xrhmy8txw?svg=true)](https://ci.appveyor.com/project/marinagagarina/2-1b)
---
+++ 2.1b/unit/src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,43 @@
+package ru.netology.unitTestCI;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CashbackHackServiceTest {
+    @Test
+    void shouldReturn100IfAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturn0IfAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldThrowsExceptionIfAmountIsZero() {
+        CashbackHackService service = new CashbackHackService();
+
+        assertThrows(IllegalArgumentException.class, () ->  service.remain(0));
+    }
+    @Test
+    void shouldReturnExceptionIfAmountLessThanZero() {
+        CashbackHackService service = new CashbackHackService();
+        assertThrows(IllegalArgumentException.class, () -> service.remain(-5));
+    }
+}
